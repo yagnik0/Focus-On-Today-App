4b0a283020d6308e2ae13cfc5a3c6d7c169b235b
yagnik0 <yashyagnik456@.com>
2024-03-10 17:36:57 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,28 @@
+"use strict";
+
+const checkBoxList = document.querySelectorAll(".custom-checkbox");
+const inputFields = document.querySelectorAll(".goal-input");
+const errorLabel = document.querySelector(".error-label");
+const progressBar = document.querySelector(".progress-bar")
+const progressValue = document.querySelector(".progress-value")
+
+checkBoxList.forEach((checkbox) => {
+  checkbox.addEventListener("click", (e) => {
+    const allGoalsAdded = [...inputFields].every((input) => {
+      return input.value;
+    });
+    console.log(allGoalsAdded);
+    if (allGoalsAdded) {
+      checkbox.parentElement.classList.toggle("completed");
+      progressValue.style.width = "33.33%  "
+    } else {
+      progressBar.classList.add('show-error')
+    }
+  });
+});
+
+inputFields.forEach((input) => {
+    input.addEventListener("focus", () => {
+        progressBar.classList.remove('show-error')
+    })
+})
